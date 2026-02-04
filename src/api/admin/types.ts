25ab8f351b3f5b8573b048bab958dfe41658276e
Kimerland <kimerland.project@gmail.com>
2026-02-05 00:50:27 +0300

    feat(updated): created modal template & dashboard

--- src/api/admin/types.ts
+++ src/api/admin/types.ts
@@ -2,3 +2,31 @@ export interface IModerateData {
   decision: string;
   feedback: string;
 }
+
+export interface ICreateTask {
+  title: string;
+}
+
+export interface IUpdateTask {
+  title: string;
+  status: string;
+}
+
+export interface ITask {
+  id: string;
+  title: string;
+  status: "DONE" | "IN_PROGRESS" | "TODO";
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface IDashboardStats {
+  totalUser: number | string;
+  totalArticles: number | string;
+  newMessages: number | string;
+}
+
+export interface IDashboardData {
+  stats: IDashboardStats;
+  missions: ITask[];
+}
